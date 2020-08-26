23684372e9225a5a4f8e31a12139f219fd718666
Rafael Alonso <rafael.palonso@gmail.com>
2020-08-26 11:03:40 -0300

    Extra features

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -15,15 +16,30 @@ const initMapbox = () => {
     // parte de criar o mapa no nosso HTML
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/rafaelalonso42/ckealc00l03ck19o0ia0x2tk9'
     });
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                        mapboxgl: mapboxgl }));
 
     // parte de adicionar os markers no nosso map
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
+      // create a Popup for the marker, with the HTML stored in infoWindow
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '30px';
+
+      new mapboxgl.Marker(element)             // create the marker
+        .setLngLat([ marker.lng, marker.lat ]) // where it's located
+        .setPopup(popup)                       // set the Popup
+        .addTo(map);                           // insert it in the map
     });
 
     // centraliza o mapa nos meus markers
